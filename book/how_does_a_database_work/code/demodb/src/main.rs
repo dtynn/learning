c4092457c1ed2d4a72652eed0efc7d33040c7ab5
dtynn <dtynn@163.com>
2019-03-29 11:11:20 +0800

    how does a database work: part 2

--- book/how_does_a_database_work/code/demodb/src/main.rs
+++ book/how_does_a_database_work/code/demodb/src/main.rs
@@ -5,28 +5,131 @@ fn main() {
     let stdin = io::stdin();
     let stdout = io::stdout();
     let mut stdin_lock = stdin.lock();
-    let mut stdout_lock = stdout.lock();
+    let mut ctx = Context {
+        stdout: stdout.lock(),
+    };
     let mut line_buf = String::new();
 
     loop {
-        print_prompt(&mut stdout_lock);
+        ctx.print_prompt().unwrap();
 
         stdin_lock.read_line(&mut line_buf).unwrap();
 
-        match line_buf.trim_end() {
-            ".exit" => {
-                println!("bye!");
-                exit(0)
-            }
-            cmd => println!("Unrecognized comman '{}'", cmd),
+        let s = line_buf.trim();
+        if !s.is_empty() {
+            let cmd = Command::from_str(s);
+            cmd.exec(&mut ctx);
         }
 
         line_buf.clear();
     }
 }
 
-fn print_prompt<W: Write>(w: &mut W) {
-    w.write_all("db > ".as_bytes())
-        .and_then(|_| w.flush())
-        .unwrap();
+enum MetaCommand {
+    Exit,
+    Unrecognized(String),
+}
+
+impl MetaCommand {
+    fn from_str(cmd: &str) -> Self {
+        match cmd.to_lowercase().as_str() {
+            ".exit" => MetaCommand::Exit,
+            _ => MetaCommand::Unrecognized(cmd.to_owned()),
+        }
+    }
+
+    fn exec(&self, ctx: &mut Context) {
+        match self {
+            MetaCommand::Exit => {
+                ctx.write_line_to_out("bye!").unwrap();
+                exit(0);
+            }
+
+            MetaCommand::Unrecognized(raw) => {
+                ctx.write_line_to_out(&format!("Unrecognized meta command '{}'", raw))
+                    .unwrap();
+            }
+        }
+    }
+}
+
+enum Statement {
+    Select,
+    Insert,
+    Unrecognized,
+}
+
+impl Statement {
+    fn from_str(s: &str) -> Self {
+        let st = s.to_lowercase();
+        if st.starts_with("select") {
+            return Statement::Select;
+        }
+
+        if st.starts_with("insert") {
+            return Statement::Insert;
+        }
+
+        Statement::Unrecognized
+    }
+
+    fn exec(&self, ctx: &mut Context) {
+        match self {
+            Statement::Select => {
+                ctx.write_line_to_out("Executing SELECT statement").unwrap();
+            }
+
+            Statement::Insert => {
+                ctx.write_line_to_out("Executing INSERT statement").unwrap();
+            }
+
+            Statement::Unrecognized => {
+                ctx.write_line_to_out("Unrecognized statement").unwrap();
+            }
+        }
+    }
+}
+
+enum Command {
+    MetaCommand(MetaCommand),
+    Statement(Statement),
+}
+
+impl Command {
+    fn from_str(s: &str) -> Self {
+        if s.starts_with(".") {
+            Command::MetaCommand(MetaCommand::from_str(s))
+        } else {
+            Command::Statement(Statement::from_str(s))
+        }
+    }
+
+    fn exec(&self, ctx: &mut Context) {
+        match self {
+            Command::MetaCommand(mc) => mc.exec(ctx),
+            Command::Statement(st) => st.exec(ctx),
+        }
+    }
+}
+
+struct Context<'a> {
+    stdout: io::StdoutLock<'a>,
+}
+
+const CR: [u8; 1] = [b'\n'];
+const PROMPT: &str = "db > ";
+
+impl<'a> Context<'a> {
+    fn print_prompt(&mut self) -> io::Result<()> {
+        self.stdout
+            .write_all(PROMPT.as_bytes())
+            .and_then(|_| self.stdout.flush())
+    }
+
+    fn write_line_to_out(&mut self, s: &str) -> io::Result<()> {
+        self.stdout
+            .write_all(s.as_bytes())
+            .and_then(|_| self.stdout.write_all(&CR[..]))
+            .and_then(|_| self.stdout.flush())
+    }
 }
