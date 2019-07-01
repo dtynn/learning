57b2a59cf231b18f5c78c2c1e5edd2ba5a6183e9
dtynn <dtynn@163.com>
2019-07-01 14:57:32 +0800

    book: algorithm by jeff, exercise: ch03_q004

--- book/algorithm_by_jeff_e/exercises/codes/ch03_q004/src/lib.rs
+++ book/algorithm_by_jeff_e/exercises/codes/ch03_q004/src/lib.rs
@@ -3,83 +3,34 @@ pub struct SubA {}
 impl SubA {
     // 需要根据切分结果, 去重
     pub fn partitions(s: &str) -> usize {
-        let mut can_split_memo: std::collections::HashMap<&[u8], bool> =
-            std::collections::HashMap::new();
+        SubA::partitions_for_bytes(s.as_bytes())
+    }
 
-        let bytes = s.as_bytes();
-        let size = bytes.len();
-        if size == 0 {
+    fn partitions_for_bytes(b: &[u8]) -> usize {
+        if b.is_empty() {
             return 0;
         }
 
         let mut count = 0;
-        if SubA::is_word(bytes) {
+        if SubA::is_word(b) {
             count += 1;
         }
 
-        for i in 1..size {
-            let (left, right) = bytes.split_at(i);
-            if SubA::can_split(left, &mut can_split_memo)
-                && SubA::can_split(right, &mut can_split_memo)
-            {
-                println!(
-                    "split: {:?}, {:?}",
-                    String::from_utf8_lossy(left),
-                    String::from_utf8_lossy(right)
-                );
-                count += 1;
+        for split_at in 1..b.len() {
+            let (left, right) = (&b[0..split_at], &b[split_at..]);
+            if SubA::is_word(left) {
+                count += SubA::partitions_for_bytes(right);
             }
-        }
-
-        println!("memo:");
-        for (k, v) in can_split_memo {
-            println!("\t key={}, can={}", String::from_utf8_lossy(k), v);
         }
 
         count
-    }
-
-    fn can_split<'a, 'b: 'a>(
-        bytes: &'b [u8],
-        memo: &mut std::collections::HashMap<&'a [u8], bool>,
-    ) -> bool {
-        let size = bytes.len();
-        if size == 0 {
-            return false;
-        }
-
-        if let Some(can) = memo.get(bytes).cloned() {
-            return can;
-        }
-
-        let is_word = SubA::is_word(bytes);
-        memo.insert(&bytes, is_word);
-        if is_word {
-            return true;
-        }
-
-        let mut can = false;
-        for i in 1..size {
-            let (left, right) = bytes.split_at(i);
-            if SubA::can_split(left, memo) && SubA::can_split(right, memo) {
-                println!(
-                    "check: left={:?}, right={:?}",
-                    String::from_utf8_lossy(left),
-                    String::from_utf8_lossy(right)
-                );
-                can = true;
-                break;
-            }
-        }
-
-        memo.insert(&bytes, can);
-        can
     }
 
     fn is_word(s: &[u8]) -> bool {
         match s {
             b"ARTIST" | b"OIL" => true,
             b"ART" | b"IS" | b"TOIL" => true,
+            b"TO" | b"IL" => true,
             _ => false,
         }
     }
@@ -89,6 +40,6 @@ impl SubA {
 mod tests {
     #[test]
     fn sub_a_test() {
-        assert_eq!(super::SubA::partitions("ARTISTOIL"), 2);
+        assert_eq!(super::SubA::partitions("ARTISTOIL"), 3);
     }
 }
